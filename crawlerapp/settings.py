bc04b9409a1802b7ae6f9b5bc1886c0268675b99
Josephshitandi <jsphshtnd.com>
2022-01-25 18:24:12 +0300

    Defining MODE and DEBUG in settings

--- crawlerapp/settings.py
+++ crawlerapp/settings.py
@@ -95,7 +95,7 @@ WSGI_APPLICATION = 'crawlerapp.wsgi.application'
 #     }
 # }
 
-MODE=config("MODE", default="dev")
+MODE = config("MODE", default="dev")
 SECRET_KEY = '+&3ku+_xv!p-5+038*0j^t(7_-r0f^=s4+n^hn+2xq7y+z)4=y'
 DEBUG = config('DEBUG', default=False, cast=bool)
 
