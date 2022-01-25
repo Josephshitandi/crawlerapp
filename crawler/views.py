fb52bc0036d8d73e2377401ee551693d8e2451bb
Josephshitandi <jsphshtnd.com>
2022-01-25 16:23:10 +0300

    Added Procfile

--- crawler/views.py
+++ crawler/views.py
@@ -3,28 +3,29 @@ from crawler.models import Headline
 import requests
 from django.shortcuts import render, redirect
 from bs4 import BeautifulSoup as BSoup
+from requests_html import HTMLSession
 
 # Create your views here.
 
 
 def scrape(request):
     Headline.objects.all().delete()
-    session = requests.Session()
+    session = HTMLSession()
     session.headers = {
         "User-Agent": "Googlebot/2.1 (+http://www.google.com/bot.html)"}
     url = "https://www.theonion.com/latest"
     content = session.get(url).content
     soup = BSoup(content, "html.parser")
-    News = soup.find_all('div', {"class": "cw4lnv-11 dFCKPx"})
+    News = soup.find_all('div', {"class": "cw4lnv-11 dFCKPx"},{"src":True})
+    print("These are...................",News)
     for article in News:
         main = article.find_all('a', href=True)
         linkx = article.find('a', {"class": "sc-1out364-0 hMndXN js_link"})
         link = linkx['href']
-        imgx = main[0].find('img', src=True)
-        image_src = imgx['srcset'].split(" ")[-4]
-        titlex = article.find(
-            'h2', {"class": "sc-759qgu-0 cYlVdn cw4lnv-6 eXwNRE"})
-        title = titlex.text
+        imgx = main[0].find('img',{"src":True})
+        image_src = imgx['src'].split('/')[-1]
+        titlex = article.find('h2', {"class": "sc-759qgu-0 cYlVdn cw4lnv-6 eXwNRE"}).text
+        title = titlex
         new_headline = Headline()
         new_headline.title = title
         new_headline.url = link
--- crawlerapp/settings.py
+++ crawlerapp/settings.py
@@ -14,6 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+TEMPLATE_DIR=os.path.join(BASE_DIR,'crawler/templates/news').replace('\\','/')
 
 
 # Quick-start development settings - unsuitable for production
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crawler'
 ]
 
 MIDDLEWARE = [
@@ -73,10 +75,18 @@ WSGI_APPLICATION = 'crawlerapp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'crawler',
+        'USER': 'shitandi',
+    'PASSWORD':'junior',
     }
 }
 
@@ -105,7 +115,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
