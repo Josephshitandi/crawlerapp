0abb2227254c2ee4a24eb7cb75eb785a646b210f
Josephshitandi <jsphshtnd.com>
2022-01-25 11:31:56 +0300

    Added a scrape view function that scrapes news from url

--- crawler/views.py
+++ crawler/views.py
@@ -5,3 +5,12 @@ from django.shortcuts import render, redirect
 from bs4 import BeautifulSoup as BSoup    
 
 # Create your views here.
+
+def scrape(request):
+  Headline.objects.all().delete()
+  session = requests.Session()
+  session.headers = {"User-Agent": "Googlebot/2.1 (+http://www.google.com/bot.html)"}
+  url = "https://www.theonion.com/latest"
+  content = session.get(url).content
+  soup = BSoup(content, "html.parser")
+  News = soup.find_all('div', {"class":"cw4lnv-11 dFCKPx"})
