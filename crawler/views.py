2ad522e7f357e071da0a2123ea0a8188ea4ad6fe
Josephshitandi <jsphshtnd.com>
2022-01-25 11:40:04 +0300

    Added method that loops through the data obtained from url, finds and returns the title,link and image if available and saves them

--- crawler/views.py
+++ crawler/views.py
@@ -1,16 +1,33 @@
 from django.shortcuts import render
-from crawler.models import Headline 
+from crawler.models import Headline
 import requests
 from django.shortcuts import render, redirect
-from bs4 import BeautifulSoup as BSoup    
+from bs4 import BeautifulSoup as BSoup
 
 # Create your views here.
 
+
 def scrape(request):
-  Headline.objects.all().delete()
-  session = requests.Session()
-  session.headers = {"User-Agent": "Googlebot/2.1 (+http://www.google.com/bot.html)"}
-  url = "https://www.theonion.com/latest"
-  content = session.get(url).content
-  soup = BSoup(content, "html.parser")
-  News = soup.find_all('div', {"class":"cw4lnv-11 dFCKPx"})
+    Headline.objects.all().delete()
+    session = requests.Session()
+    session.headers = {
+        "User-Agent": "Googlebot/2.1 (+http://www.google.com/bot.html)"}
+    url = "https://www.theonion.com/latest"
+    content = session.get(url).content
+    soup = BSoup(content, "html.parser")
+    News = soup.find_all('div', {"class": "cw4lnv-11 dFCKPx"})
+    for article in News:
+        main = article.find_all('a', href=True)
+        linkx = article.find('a', {"class": "sc-1out364-0 hMndXN js_link"})
+        link = linkx['href']
+        imgx = main[0].find('img', src=True)
+        image_src = imgx['srcset'].split(" ")[-4]
+        titlex = article.find(
+            'h2', {"class": "sc-759qgu-0 cYlVdn cw4lnv-6 eXwNRE"})
+        title = titlex.text
+        new_headline = Headline()
+        new_headline.title = title
+        new_headline.url = link
+        new_headline.image = image_src
+        new_headline.save()
+    return redirect("../")
