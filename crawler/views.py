884334870c75015776d1b2e5833a83fb2d05bdaf
Josephshitandi <jsphshtnd.com>
2022-01-26 06:53:59 +0300

    Modified scrape function

--- crawler/views.py
+++ crawler/views.py
@@ -17,14 +17,14 @@ def scrape(request):
     content = session.get(url).content
     soup = BSoup(content, "html.parser")
     News = soup.find_all('div', {"class": "cw4lnv-11 dFCKPx"},{"src":True})
-    print("These are...................",News)
+    # print("These are...................",News)
     for article in News:
         main = article.find_all('a', href=True)
         linkx = article.find('a', {"class": "sc-1out364-0 hMndXN js_link"})
         link = linkx['href']
-        imgx = main[0].find('img',{"src":True})
-        image_src = imgx['src'].split('/')[-4]
-        titlex = article.find('h2', {"class": "sc-759qgu-0 cYlVdn cw4lnv-6 eXwNRE"})
+        imgx = main[0].find('img',src=True)
+        image_src = imgx['srcset'].split(" ")[-4]
+        titlex = article.find('h2', {"class":"sc-759qgu-0 cYlVdn cw4lnv-6 eXwNRE"})
         title = titlex
         new_headline = Headline()
         new_headline.title = title
